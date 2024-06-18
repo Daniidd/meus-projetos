0fd98612897f5688da181540a2b0f5256842bbc9
Daniidd <daniela.dallacorte1@gmail.com>
2024-06-17 22:32:48 -0300

    Arrumando exercicio de Composition

--- exercicio 4 - composition/script.js
+++ exercicio 4 - composition/script.js
@@ -1,84 +1,91 @@
-   class Bateria {
-            constructor(capacidade, tipoDaBateria) {
-                this.capacidade = capacidade; // em miliampere-hora
-                this.tipoDaBateria = tipoDaBateria;
-            }
+class Bateria {
+    #capacidade;
+    #tipo;
 
-            detalhes() {
-                return `Bateria de ${this.capacidade} mAh, tipo ${this.tipoDaBateria}.`;
-            }
-        }
+    constructor(capacidade, tipo) {
+        this.#capacidade = capacidade; // em mAh
+        this.#tipo = tipo;
+    }
 
-        class Display {
-            constructor(tipoDaTela, resolucao) {
-                this.tipoDaTela = tipoDaTela;
-                this.resolucao = resolucao;
-            }
+    detalhes() {
+        return `Bateria de ${this.#capacidade} mAh, tipo ${this.#tipo}.`;
+    }
+}
 
-            detalhes() {
-                return `Display ${this.tipoDaTela} com resolução de ${this.resolucao}.`;
-            }
-        }
+class Tela {
+    #tamanho;
+    #tipoDisplay;
+    #resolucaoDisplay;
 
-        class Tela {
-            constructor(tamanho, display) {
-                this.tamanho = tamanho; // em polegadas
-                this.display = display;
-            }
+    constructor(tamanho, tipoDisplay, resolucaoDisplay) {
+        this.#tamanho = tamanho; // em polegadas
+        this.#tipoDisplay = tipoDisplay;
+        this.#resolucaoDisplay = resolucaoDisplay;
+    }
 
-            detalhes() {
-                return `Tela de ${this.tamanho} polegadas.<br>` +
-                       `${this.display.detalhes()}`;
-            }
-        }
+    detalhes() {
+        return `Tela de ${this.#tamanho} polegadas, com display ${this.#tipoDisplay} e resolução de ${this.#resolucaoDisplay}.`;
+    }
+}
 
-        class Camera {
-            constructor(resolucao, lenteTipo) {
-                this.resolucao = resolucao; // em megapixels
-                this.lenteTipo = lenteTipo;
-            }
+class Camera {
+    #resolucao;
+    #lenteTipo;
 
-            detalhes() {
-                return `Câmera de ${this.resolucao} MP, lente ${this.lenteTipo}.`;
-            }
-        }
+    constructor(resolucao, lenteTipo) {
+        this.#resolucao = resolucao; // em megapixels
+        this.#lenteTipo = lenteTipo;
+    }
 
-        class Processador {
-            constructor(marca, nucleos, velocidade) {
-                this.marca = marca;
-                this.nucleos = nucleos; // quantidade de núcleos
-                this.velocidade = velocidade; // em GigaHertz
-            }
+    detalhes() {
+        return `Câmera de ${this.#resolucao} MP, lente ${this.#lenteTipo}.`;
+    }
+}
 
-            detalhes() {
-                return `Processador ${this.marca} com ${this.nucleos} núcleos a ${this.velocidade} GHz.`;
-            }
-        }
+class Processador {
+    #marca;
+    #nucleos;
+    #velocidade;
 
-        class Celular {
-            constructor(marca, modelo) {
-                this.marca = marca;
-                this.modelo = modelo;
-                this.bateria = new Bateria(4441, 'Lítio');
-                this.tela = new Tela(6.7, new Display('OLED', '2796 x 1290'));
-                this.camera = new Camera(48, 'Grande Angular');
-                this.processador = new Processador('A17 Pro', 6, 3.46);
-            }
+    constructor(marca, nucleos, velocidade) {
+        this.#marca = marca;
+        this.#nucleos = nucleos; // quantidade de núcleos
+        this.#velocidade = velocidade; // em GHz
+    }
 
-            detalhes() {
-                return `Celular ${this.marca} ${this.modelo}:<br>` +
-                       `${this.bateria.detalhes()}<br>` +
-                       `${this.tela.detalhes()}<br>` +
-                       `${this.camera.detalhes()}<br>` +
-                       `${this.processador.detalhes()}`;
-            }
-        }
+    detalhes() {
+        return `Processador ${this.#marca} com ${this.#nucleos} núcleos a ${this.#velocidade} GHz.`;
+    }
+}
 
-        // Criando um cel
-        const celular1 = new Celular('Iphone', '15 Pro Max');
+class Celular {
+    #marca;
+    #modelo;
+    #bateria;
+    #tela;
+    #camera;
+    #processador;
 
-        // Exibindo detalhes do cel na pág
-        document.getElementById('detalhes-celular').innerHTML = celular1.detalhes();
-    
+    constructor(marca, modelo) {
+        this.#marca = marca;
+        this.#modelo = modelo;
+        this.#bateria = new Bateria(4441, 'Lítio');
+        this.#tela = new Tela(6.7, 'OLED', '2796 x 1290');
+        this.#camera = new Camera(48, 'Grande Angular');
+        this.#processador = new Processador('A17 Pro', 6, 3.46);
+    }
 
+    detalhes() {
+        return `Celular ${this.#marca} ${this.#modelo}:<br>` +
+            `${this.#bateria.detalhes()}<br>` +
+            `${this.#tela.detalhes()}<br>` +
+            `${this.#camera.detalhes()}<br>` +
+            `${this.#processador.detalhes()}`;
+    }
+}
 
+// Criando um cel
+const celular1 = new Celular('Iphone', '15 Pro Max');
+
+// Exibindo detalhes do cel na pág
+document.getElementById('detalhes-celular').innerHTML = celular1.detalhes();
